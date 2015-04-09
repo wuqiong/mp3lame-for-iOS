6dd90d7c4d073d9af599e485d1aa32f173426f0f
Sunny <sipdar@163.com>
2015-04-09 16:22:56 +0800

    first release for cocoapods

---
+++ Example/Tests/Tests.m
@@ -0,0 +1,9 @@
+//
+//  mp3lame-for-iosTests.m
+//  mp3lame-for-iosTests
+//
+//  Created by Sunny on 04/09/2015.
+//  Copyright (c) 2014 Sunny. All rights reserved.
+//
+
+${TEST_EXAMPLE}
---
+++ Example/mp3lame-for-ios/MP3LameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MP3LameAppDelegate.h
+//  mp3lame-for-ios
+//
+//  Created by CocoaPods on 04/09/2015.
+//  Copyright (c) 2014 Sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MP3LameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
